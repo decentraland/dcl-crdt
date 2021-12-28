74ab6fa6e46a4bfa14517f9de6e446c88147947a
gonpombo8 <gonzalo@decentraland.org>
2021-12-28 17:01:20 -0300

    reorder test

--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -16,14 +16,23 @@ function compareStatePayloads(states: State<Buffer>[]) {
 
 describe('CRDT protocol', () => {
   it('smoke test', async () => {
+    function compare() {
+      expect(
+        compareStatePayloads([
+          server.getState(),
+          ...clients.map((c) => c.getState())
+        ])
+      ).toBe(true)
+    }
+
+
+    // Kind of server. We only receive messages to store the state.
     const ws = {
       send: async (message: Message<Buffer>) => {
         await server.processMessage(message)
         await Promise.all(clients.map((c) => c.processMessage(message)))
       }
     }
-
-    // Kind of server. We only receive messages to store the state.
     const server = crdtProtocol<Buffer>(async (_message: Message<Buffer>) => {})
 
     // All the clients that can send messages
@@ -34,22 +43,14 @@ describe('CRDT protocol', () => {
     const clientB = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
       await ws.send(message)
     })
-
     const clientC = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
       await ws.send(message)
     })
-
     clients.push(clientA, clientB, clientC)
+
+    // Start sending messages
     const messageA = clientA.createEvent('key-A', Buffer.from('casla'))
     await clientA.sendMessage(messageA.key, messageA.data)
-    function compare() {
-      expect(
-        compareStatePayloads([
-          server.getState(),
-          ...clients.map((c) => c.getState())
-        ])
-      ).toBe(true)
-    }
     compare()
     const messageB = clientA.createEvent('key-A', Buffer.from('boedo'))
     await clientB.sendMessage(messageB.key, messageB.data)
