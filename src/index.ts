281f122108746d1765a91b89ad4c817c824b92c6
gonpombo8 <gonzalo@decentraland.org>
2021-12-30 01:50:37 -0300

    crdt with client & server use cases

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,36 @@ import { Message, Payload, SendUpdates, State } from './types'
 export * from './types'
 
 /**
+ * Compare raw data.
+ */
+function sameData<T = unknown>(a: T, b: T) {
+  if (a instanceof Buffer && b instanceof Buffer) {
+    return a.equals(b)
+  }
+  return a === b
+}
+
+/**
  * @public
  * CRDT protocol.
  * Stores the latest state, and decides whenever we have
  * to process and store the new data in case its an update, or
  * to discard and send our local value cause remote it's outdated.
  */
-export function crdtProtocol<T>(sendUpdates: SendUpdates<T>) {
+export function crdtProtocol<T>(sendUpdates: SendUpdates<T>, id: string) {
+  /**
+   * UUID identifier
+   */
+  const uuid = id
+
+  /**
+   * Logger
+   */
+  function log(...args: any) {
+    return
+    console.log(`${uuid}: ${JSON.stringify(args, null)}`)
+  }
+
   /**
    * Local state where we store the latest lamport timestamp
    * and the raw data value
@@ -16,14 +39,15 @@ export function crdtProtocol<T>(sendUpdates: SendUpdates<T>) {
   let state: State<T> = {}
   /**
    * Every time we create/receive/send a message
-   * we should call this fn in order to update the state with the new payload.
+   * we should call this fn in order to update the state
+   * and increment the counter by one
    */
   function tickEvent(
     key: string,
     data: T,
     remoteTimestamp?: number
   ): Payload<T> {
-    const current = remoteTimestamp || state[key]?.timestamp || 0
+    const current = Math.max(remoteTimestamp || 0, state[key]?.timestamp || 0)
 
     return (state[key] = {
       timestamp: current + 1,
@@ -32,8 +56,8 @@ export function crdtProtocol<T>(sendUpdates: SendUpdates<T>) {
   }
 
   /**
-   * Create an event for the specified key and store the new
-   * lamport timestmap and data in the store state.
+   * Create an event for the specified key and store the new data and
+   * lamport timestmap incremented by one in the state.
    */
   function createEvent(key: string, data: T): Message<T> {
     const message = tickEvent(key, data)
@@ -41,47 +65,64 @@ export function crdtProtocol<T>(sendUpdates: SendUpdates<T>) {
   }
 
   /**
-   *
+   * Send generated message, and increment the counter by one
    */
-  function sendMessage(key: string, data: T) {
-    const event = tickEvent(key, data)
-    const message = { ...event, key }
-    return sendUpdates(message)
+  function sendMessage(message: Message<T>) {
+    const { timestamp } = tickEvent(message.key, message.data)
+    return sendUpdates({ ...message, timestamp })
   }
 
   /**
    * Process the received message only if the lamport number is higher than
    * the current one. If not, seems we have a race condition.
-   * The bigger raw data wins, and spreads the network with this new value.
+   * The bigger raw data wins, and spreads it to the network
    */
   function processMessage(message: Message<T>) {
     const { key, data, timestamp } = message
     const current = state[key]
-
+    log(current, message)
     // Somehow the message that we sent came back as an echo.
-    if (current?.timestamp === timestamp && current?.data === data) {
+    if (sameData(current?.data, data)) {
+      log('same data')
       return
     }
 
     // If the received timestamp is > than our current value,
     // store the new payload and increment our lamport timestamp by one
     if (!current || current.timestamp < timestamp) {
+      log('tick event', data, timestamp)
       return tickEvent(key, data, timestamp)
     }
 
     // If our current timestamp is higher, then send the message
-    // to the network with our payload.
+    // to the network with our payload without incrementing the counter.
     if (current.timestamp > timestamp) {
-      return sendMessage(key, current.data)
+      log('send updates')
+      // return sendMessage({
+      //   key,
+      //   data: current.data,
+      //   timestamp: current.timestamp
+      // })
+      return sendUpdates({
+        key,
+        timestamp: current.timestamp,
+        data: current.data
+      })
     }
 
     // if both timestamps are equal, then we have a race condition.
     // We should compare the raw data and the higher one wins.
+    // We MUST increment the counter.
     function compareData(current: unknown, data: unknown) {
       return (current as number) > (data as number)
     }
+    log(
+      'compare data',
+      compareData(current.data, data) ? current : data,
+      timestamp
+    )
     return compareData(current.data, data)
-      ? sendMessage(key, current.data)
+      ? sendMessage({ key, data: current.data, timestamp: current.timestamp })
       : tickEvent(key, data, timestamp)
   }
 
@@ -99,11 +140,19 @@ export function crdtProtocol<T>(sendUpdates: SendUpdates<T>) {
     return (state = {}) as State<T>
   }
 
+  /**
+   * Returns the uuid
+   */
+  function getUUID(): string {
+    return uuid
+  }
+
   return {
     createEvent,
     sendMessage,
     processMessage,
     getState,
+    getUUID,
     clearState
   }
 }
--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -1,6 +1,10 @@
 import { crdtProtocol, Message, State } from '../src'
 import expect from 'expect'
 
+function compareData(a: Buffer, b: Buffer) {
+  return a.equals(b)
+}
+
 function compareStatePayloads(states: State<Buffer>[]) {
   if (!states.length) {
     return true
@@ -10,50 +14,143 @@ function compareStatePayloads(states: State<Buffer>[]) {
   return states.every(
     (s) =>
       s.length === states[0].length &&
-      keys.every((key) => s[key].data.equals(states[0][key].data))
+      keys.every((key) => compareData(s[key].data, states[0][key].data))
   )
 }
 
+async function voidFn() {}
+
+type Sandbox = {
+  clientLength: number
+}
+
+function createSandbox(opts: Sandbox) {
+  let id = 0
+  const getId = () => {
+    id = id + 1
+    return id.toString()
+  }
+  const createWs = (uuid: string) => ({
+    send: async (message: Message<Buffer>) => {
+      await server.processMessage(message)
+      await Promise.all(
+        clients.map((c) => c.getUUID() !== uuid && c.processMessage(message))
+      )
+    }
+  })
+
+  const server = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
+    // await Promise.all(clients.map((c) => c.processMessage(message)))
+  }, getId())
+  const clients: typeof server[] = Array.from({
+    length: opts.clientLength
+  }).map(() => {
+    const uuid = getId()
+    const ws = createWs(uuid)
+    return crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
+      await ws.send(message)
+    }, uuid)
+  })
+
+  function compare() {
+    expect(
+      compareStatePayloads([
+        server.getState(),
+        ...clients.map((c) => c.getState())
+      ])
+    ).toBe(true)
+  }
+
+  return {
+    compare,
+    clients,
+    server
+  }
+}
+
+beforeEach(() => {})
+
 describe('CRDT protocol', () => {
-  it('smoke test', async () => {
-    function compare() {
-      expect(
-        compareStatePayloads([
-          server.getState(),
-          ...clients.map((c) => c.getState())
-        ])
-      ).toBe(true)
-    }
-
-
-    // Kind of server. We only receive messages to store the state.
-    const ws = {
-      send: async (message: Message<Buffer>) => {
-        await server.processMessage(message)
-        await Promise.all(clients.map((c) => c.processMessage(message)))
-      }
-    }
-    const server = crdtProtocol<Buffer>(async (_message: Message<Buffer>) => {})
-
-    // All the clients that can send messages
-    const clients: typeof server[] = []
-    const clientA = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
-      await ws.send(message)
-    })
-    const clientB = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
-      await ws.send(message)
-    })
-    const clientC = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
-      await ws.send(message)
-    })
-    clients.push(clientA, clientB, clientC)
-
-    // Start sending messages
+  it('should store the message A in all the clients', async () => {
+    const { clients, compare } = createSandbox({ clientLength: 2 })
+    const [clientA] = clients
     const messageA = clientA.createEvent('key-A', Buffer.from('casla'))
-    await clientA.sendMessage(messageA.key, messageA.data)
+    await clientA.sendMessage(messageA)
     compare()
-    const messageB = clientA.createEvent('key-A', Buffer.from('boedo'))
-    await clientB.sendMessage(messageB.key, messageB.data)
+  })
+
+  it('same as before but with more clients (N > 2)', async () => {
+    const { clients, compare } = createSandbox({ clientLength: 20 })
+    const [clientA] = clients
+    const messageA = clientA.createEvent('key-A', Buffer.from('casla'))
+    await clientA.sendMessage(messageA)
+    compare()
+  })
+
+  it('should decline message A if both messages are sent at the same time and data B > data A', async () => {
+    const { server, clients, compare } = createSandbox({ clientLength: 2 })
+    const [clientA, clientB] = clients
+    const key = 'key-A'
+    const messageA = clientA.createEvent(key, Buffer.from('boedo'))
+    const messageB = clientB.createEvent(key, Buffer.from('casla'))
+    await Promise.all([
+      clientA.sendMessage(messageA),
+      clientB.sendMessage(messageB)
+    ])
+    compare()
+    expect(compareData(server.getState()[key].data, messageB.data)).toBe(true)
+  })
+
+  it('same as before but with more clients (N > 2)', async () => {
+    const { server, clients, compare } = createSandbox({ clientLength: 20 })
+    const [clientA, clientB] = clients
+    const key = 'key-A'
+    const messageA = clientA.createEvent(key, Buffer.from('boedo'))
+    const messageB = clientB.createEvent(key, Buffer.from('casla'))
+    await Promise.all([
+      clientA.sendMessage(messageA),
+      clientB.sendMessage(messageB)
+    ])
+    compare()
+    expect(compareData(server.getState()[key].data, messageB.data)).toBe(true)
+  })
+
+  it('should store both keys', async () => {
+    const { clients, compare } = createSandbox({ clientLength: 2 })
+    const [clientA, clientB] = clients
+    const keyA = 'key-A'
+    const keyB = 'key-B'
+    console.log(clientA.getState())
+    const messageA = clientA.createEvent(keyA, Buffer.from('boedo'))
+    const messageB = clientB.createEvent(keyB, Buffer.from('casla'))
+    await clientA.sendMessage(messageA)
+    await clientB.sendMessage(messageB)
+    console.log(clientA.getState())
+    const messageB2 = clientB.createEvent(keyB, Buffer.from('sand'))
+    const messageA2 = clientA.createEvent(keyB, Buffer.from('holaholaholahola'))
+    console.log('MESSAGE', messageA2, messageB2)
+    await Promise.all([
+      clientB.sendMessage(messageB2),
+      clientA.sendMessage(messageA2)
+    ])
+    compare()
+  })
+
+  it('same as before but with more clients (N > 2)', async () => {
+    const { clients, compare } = createSandbox({ clientLength: 20 })
+    const [clientA, clientB, clientC] = clients
+    const keyA = 'key-A'
+    const keyB = 'key-B'
+    const messageA = clientA.createEvent(keyA, Buffer.from('boedo'))
+    const messageB = clientB.createEvent(keyB, Buffer.from('casla'))
+    await clientA.sendMessage(messageA)
+    await clientB.sendMessage(messageB)
+    const messageB2 = clientB.createEvent(keyB, Buffer.from('sand'))
+    const messageC = clientC.createEvent(keyB, Buffer.from('hola'))
+    await Promise.all([
+      clientB.sendMessage(messageB2),
+      clientC.sendMessage(messageC)
+    ])
     compare()
   })
 })
