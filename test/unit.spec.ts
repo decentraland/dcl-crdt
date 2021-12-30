84cdb0118a2b1df0b85c06d65ea13c09d3e41916
gonpombo8 <gonzalo@decentraland.org>
2021-12-30 01:55:41 -0300

    remove concept of a server that only listens. Now they are all clients that listens and resends messages if they have to

--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -18,8 +18,6 @@ function compareStatePayloads(states: State<Buffer>[]) {
   )
 }
 
-async function voidFn() {}
-
 type Sandbox = {
   clientLength: number
 }
@@ -30,41 +28,31 @@ function createSandbox(opts: Sandbox) {
     id = id + 1
     return id.toString()
   }
-  const createWs = (uuid: string) => ({
+  const broadcast = (uuid: string) => ({
     send: async (message: Message<Buffer>) => {
-      await server.processMessage(message)
       await Promise.all(
         clients.map((c) => c.getUUID() !== uuid && c.processMessage(message))
       )
     }
   })
 
-  const server = crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
-    // await Promise.all(clients.map((c) => c.processMessage(message)))
-  }, getId())
-  const clients: typeof server[] = Array.from({
+  const clients = Array.from({
     length: opts.clientLength
   }).map(() => {
     const uuid = getId()
-    const ws = createWs(uuid)
+    const ws = broadcast(uuid)
     return crdtProtocol<Buffer>(async (message: Message<Buffer>) => {
       await ws.send(message)
     }, uuid)
   })
 
   function compare() {
-    expect(
-      compareStatePayloads([
-        server.getState(),
-        ...clients.map((c) => c.getState())
-      ])
-    ).toBe(true)
+    expect(compareStatePayloads(clients.map((c) => c.getState()))).toBe(true)
   }
 
   return {
     compare,
-    clients,
-    server
+    clients
   }
 }
 
@@ -88,21 +76,23 @@ describe('CRDT protocol', () => {
   })
 
   it('should decline message A if both messages are sent at the same time and data B > data A', async () => {
-    const { server, clients, compare } = createSandbox({ clientLength: 2 })
+    const { clients, compare } = createSandbox({ clientLength: 2 })
     const [clientA, clientB] = clients
     const key = 'key-A'
+
     const messageA = clientA.createEvent(key, Buffer.from('boedo'))
     const messageB = clientB.createEvent(key, Buffer.from('casla'))
     await Promise.all([
       clientA.sendMessage(messageA),
       clientB.sendMessage(messageB)
     ])
+
     compare()
-    expect(compareData(server.getState()[key].data, messageB.data)).toBe(true)
+    expect(compareData(clientA.getState()[key].data, messageB.data)).toBe(true)
   })
 
   it('same as before but with more clients (N > 2)', async () => {
-    const { server, clients, compare } = createSandbox({ clientLength: 20 })
+    const { clients, compare } = createSandbox({ clientLength: 20 })
     const [clientA, clientB] = clients
     const key = 'key-A'
     const messageA = clientA.createEvent(key, Buffer.from('boedo'))
@@ -112,7 +102,7 @@ describe('CRDT protocol', () => {
       clientB.sendMessage(messageB)
     ])
     compare()
-    expect(compareData(server.getState()[key].data, messageB.data)).toBe(true)
+    expect(compareData(clientA.getState()[key].data, messageB.data)).toBe(true)
   })
 
   it('should store both keys', async () => {
@@ -120,18 +110,37 @@ describe('CRDT protocol', () => {
     const [clientA, clientB] = clients
     const keyA = 'key-A'
     const keyB = 'key-B'
-    console.log(clientA.getState())
+
     const messageA = clientA.createEvent(keyA, Buffer.from('boedo'))
     const messageB = clientB.createEvent(keyB, Buffer.from('casla'))
     await clientA.sendMessage(messageA)
     await clientB.sendMessage(messageB)
-    console.log(clientA.getState())
+
     const messageB2 = clientB.createEvent(keyB, Buffer.from('sand'))
     const messageA2 = clientA.createEvent(keyB, Buffer.from('holaholaholahola'))
-    console.log('MESSAGE', messageA2, messageB2)
     await Promise.all([
       clientB.sendMessage(messageB2),
       clientA.sendMessage(messageA2)
+    ])
+    compare()
+  })
+
+  it('should store both keys, even if we send the messages in diff order (sand before holahola)', async () => {
+    const { clients, compare } = createSandbox({ clientLength: 2 })
+    const [clientA, clientB] = clients
+    const keyA = 'key-A'
+    const keyB = 'key-B'
+
+    const messageA = clientA.createEvent(keyA, Buffer.from('boedo'))
+    const messageB = clientB.createEvent(keyB, Buffer.from('casla'))
+    await clientA.sendMessage(messageA)
+    await clientB.sendMessage(messageB)
+
+    const messageB2 = clientB.createEvent(keyB, Buffer.from('sand'))
+    const messageA2 = clientA.createEvent(keyB, Buffer.from('holaholaholahola'))
+    await Promise.all([
+      clientA.sendMessage(messageA2),
+      clientB.sendMessage(messageB2)
     ])
     compare()
   })
@@ -141,10 +150,12 @@ describe('CRDT protocol', () => {
     const [clientA, clientB, clientC] = clients
     const keyA = 'key-A'
     const keyB = 'key-B'
+
     const messageA = clientA.createEvent(keyA, Buffer.from('boedo'))
     const messageB = clientB.createEvent(keyB, Buffer.from('casla'))
     await clientA.sendMessage(messageA)
     await clientB.sendMessage(messageB)
+
     const messageB2 = clientB.createEvent(keyB, Buffer.from('sand'))
     const messageC = clientC.createEvent(keyB, Buffer.from('hola'))
     await Promise.all([
